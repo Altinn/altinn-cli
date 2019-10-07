e769bc1b280ec1485a9d551819caca39b220f8e1
eguneriu <35337864+eguneriu@users.noreply.github.com>
2019-10-07 13:37:23 +0200

    Intial files

---
+++ StorageClient/IStorageClientWrapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StorageClient
+{
+    public interface IStorageClientWrapper
+    {
+    }
+}
---
+++ StorageClient/StorageClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StorageClient
+{
+    public class StorageClient
+    {
+
+    }
+}
