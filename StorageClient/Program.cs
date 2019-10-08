2b1d6ab9856cfecf67d270f396da12e30a81fb78
Havard Hegtun <havard.hegtun@capgemini.com>
2019-10-08 10:56:57 +0200

    namespace changes

--- StorageClient/Program.cs
+++ StorageClient/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace StorageClient
+namespace Altinn.Clients.StorageClient
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, string> dict = args.Select(a => a.Split('=')).ToDictionary(a => a[0].ToLower().Substring(1), a => a.Length == 2 ? a[1] : null);
+
+            string appId = null;
+            string url = null;
+            bool quit = false;
+
+           if (dict.ContainsKey("appid") && dict.ContainsKey("url"))
+            { 
+                appId = dict["appid"];
+                url = dict["url"];
+            } 
         }
     }
 }
