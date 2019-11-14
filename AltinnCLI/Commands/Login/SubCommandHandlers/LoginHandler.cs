a64c511ff256d3891305bb94d62b9ada92097fa8
Erik Guneriussen <erik.guneriussen@capgemini.com>
2019-11-14 10:02:05 +0100

    Login command

---
+++ AltinnCLI/Commands/Login/SubCommandHandlers/LoginHandler.cs
@@ -0,0 +1,94 @@
+﻿using AltinnCLI.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AltinnCLI.Commands.Login.SubCommandHandlers
+{
+    public class LoginHandler : SubCommandHandlerBase, ISubCommandHandler, IHelp
+    {
+ ///       private IStorageClientWrapper ClientWrapper = null;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetDocumentHandler" /> class.
+        /// </summary>
+        /// <param name="logger">Reference to the common logger that the application shall used to log log info and error information
+        public LoginHandler(ILogger<LoginHandler> logger) : base(logger)
+        {
+
+            if (ApplicationManager.ApplicationConfiguration.GetSection("UseLiveClient").Get<bool>())
+            {
+                ClientWrapper = new StorageClientWrapper(_logger);
+            }
+            else
+            {
+                ClientWrapper = new StorageClientFileWrapper(_logger);
+            }
+        }
+
+
+        /// <summary>
+        /// Gets the name of of the command
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return "Login";
+            }
+        }
+        /// <summary>
+        /// Gets the name of the CommandProvider that uses this command
+        /// </summary>
+        public string CommandProvider
+        {
+            get
+            {
+                return "Login";
+            }
+        }
+
+        /// <summary>
+        /// Gets the description of the command handler that is used by the Help function
+        /// </summary>
+        public string Description
+        {
+            get
+            {
+                return "Logs in";
+            }
+        }
+
+        /// <summary>
+        /// Gets how the command can be specified to get documents, is used by the Help function
+        /// </summary>
+        public string Usage
+        {
+            get
+            {
+                string usage = $"\n" +
+                $"Login clentId=<client guid id> cert=c:<\cerificat\fileName> \n"+
+                $"\n" +
+                $" Required parameters for the login command \n";
+
+                foreach (IOption opt in SelectableCliOptions)
+                {
+                    usage += $"\t{opt.Name}\t\t {opt.Description} \n";
+                }
+
+
+                return usage;
+            }
+        }
+        public string GetHelp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
