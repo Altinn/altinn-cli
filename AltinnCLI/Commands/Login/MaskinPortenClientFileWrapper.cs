fb6ad52817f773b0547fbe11e0c7787a91dba6f0
Håvard Hegtun <havard.hegtun@capgemini.com>
2019-11-14 13:05:52 +0100

    added call to maskinporten in the wrapper

--- AltinnCLI/Commands/Login/MaskinPortenClientFileWrapper.cs
+++ AltinnCLI/Commands/Login/MaskinPortenClientFileWrapper.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 
 namespace AltinnCLI.Commands.Login
 {
     public class MaskinPortenClientFileWrapper : IMaskinPortenClientWrapper
     {
+        public string PostToken(FormUrlEncodedContent bearer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- AltinnCLI/Commands/Login/MaskinPortenClientWrapper.cs
+++ AltinnCLI/Commands/Login/MaskinPortenClientWrapper.cs
@@ -1,10 +1,52 @@
-﻿using System;
+﻿using AltinnCLI.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace AltinnCLI.Commands.Login
 {
-    public class MaskinPortenClientWrapper : IMaskinPortenClientWrapper
+    public class MaskinportenClientWrapper : IMaskinPortenClientWrapper
     {
+        /// <summary>
+        /// Application logger 
+        /// </summary>
+        protected static ILogger _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplicationClientWrapper" /> class.
+        /// </summary>
+        public MaskinportenClientWrapper(ILogger logger)
+        {
+            BaseAddress = ApplicationManager.ApplicationConfiguration.GetSection("MaskinportenBaseAddress").Get<string>();
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Gets or sets the base address
+        /// </summary>
+        private string BaseAddress { get; set; }
+
+        public string PostToken(FormUrlEncodedContent bearer)
+        {
+            HttpClientWrapper httpClientWrapper = new HttpClientWrapper(_logger);
+
+            string cmd = "";
+
+            Task<HttpResponseMessage> response = httpClientWrapper.PostCommand(BaseAddress, cmd, bearer);
+
+            if (response.Result.IsSuccessStatusCode)
+            {
+                return response.Result.Content.ReadAsStringAsync().Result;
+            }
+            else
+            {
+                return $@"Could not retrieve Token";
+            }
+            
+        }
     }
 }
