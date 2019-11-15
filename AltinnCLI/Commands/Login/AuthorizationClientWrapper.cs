624ae6d420d2d5f90bc6350d3e98fd62c545cfbd
Håvard Hegtun <havard.hegtun@capgemini.com>
2019-11-15 07:45:55 +0100

    Added empty authorization wrapper for converting maskinporten tokens to Altinn tokens

---
+++ AltinnCLI/Commands/Login/AuthorizationClientWrapper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AltinnCLI.Commands.Login
+{
+    class AuthorizationClientWrapper : IAutorizationClientWrapper
+    {
+        /// <summary>
+        /// Application logger 
+        /// </summary>
+        protected static ILogger _logger;
+
+        /// <summary>
+        /// Gets or sets the base address
+        /// </summary>
+        private string BaseAddress { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplicationClientWrapper" /> class.
+        /// </summary>
+        public AuthorizationClientWrapper(ILogger logger)
+        {
+            BaseAddress = ApplicationManager.ApplicationConfiguration.GetSection("MaskinportenBaseAddress").Get<string>();
+            _logger = logger;
+        }
+
+        public string ConvertToken(string token)
+        {
+            return "";
+        }
+    }
+}
