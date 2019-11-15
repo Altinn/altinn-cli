e19037f07723c2b32e244271a30e6f6b0b405743
Erik Guneriussen <erik.guneriussen@capgemini.com>
2019-11-15 08:42:22 +0100

    Local store connection

--- AltinnCLI/Commands/Login/SubCommandHandlers/MaskinportenLoginHandler.cs
+++ AltinnCLI/Commands/Login/SubCommandHandlers/MaskinportenLoginHandler.cs
@@ -1,18 +1,21 @@
 ﻿using AltinnCLI.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace AltinnCLI.Commands.Login.SubCommandHandlers
 {
     public class MaskinportenLoginHandler : SubCommandHandlerBase, ISubCommandHandler, IHelp
     {       
-        private MaskinPortenClientWrapper ClientWrapper = null;
+        private MaskinportenClientWrapper ClientWrapper = null;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="GetDocumentHandler" /> class.
@@ -23,11 +26,11 @@ namespace AltinnCLI.Commands.Login.SubCommandHandlers
 
             if (ApplicationManager.ApplicationConfiguration.GetSection("UseLiveClient").Get<bool>())
             {
-                ClientWrapper = new MaskinPortenClientWrapper(_logger);
+                ClientWrapper = new MaskinportenClientWrapper(_logger);
             }
             else
             {
-                ClientWrapper = new MaskinPortenClientWrapper(_logger);
+                ClientWrapper = new MaskinportenClientWrapper(_logger);
             }
         }
 
@@ -72,7 +75,7 @@ namespace AltinnCLI.Commands.Login.SubCommandHandlers
             get
             {
                 string usage = $"\n" +
-                $@"Login clentId=<client guid id> cert=<c:\cerificat\fileName> \n"+
+                $@"Login clientId=<client guid id> thumbprint=<c:\cerificat\fileName> \n" +
                 $"\n" +
                 $" Required parameters for the login command \n";
 
@@ -92,44 +95,45 @@ namespace AltinnCLI.Commands.Login.SubCommandHandlers
 
         public bool Run()
         {
+            FindAllCeriticates();
             if (IsValid)
             {
                 IOption fileNameOption = SelectableCliOptions.FirstOrDefault(x => string.Equals(x.Name, "file", StringComparison.OrdinalIgnoreCase));
 
-                FileStream stream = new FileStream(fileNameOption.Value, FileMode.Open);
-                MemoryStream memstr = new MemoryStream(new byte[stream.Length]);
-                stream.CopyTo(memstr);
-                memstr.Position = 0;
-                stream.Close();
+                string jwtAssertion = GetJwtAssertion();
 
-                jwttoken
 
- //               InstanceResponseMessage responsMessage = ClientWrapper.UploadDataElement(SelectableCliOptions, memstr, fileNameOption.Value);
+                if (!string.IsNullOrEmpty(jwtAssertion))
+                {
+                    FormUrlEncodedContent content = GetUrlEncodedContent(jwtAssertion);
+                    string token = ClientWrapper.PostToken(content);
+                }
+
+                return true;
             }
 
-            return true;
+            return false;
 
         }
 
-        public string GetAccessToken(string assertion, out bool isError)
+        public FormUrlEncodedContent GetUrlEncodedContent(string assertion)
         {
-            var formContent = new FormUrlEncodedContent(new List<KeyValuePair<string, string>>
+            FormUrlEncodedContent formContent = new FormUrlEncodedContent(new List<KeyValuePair<string, string>>
             {
                 new KeyValuePair<string, string>("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer"),
                 new KeyValuePair<string, string>("assertion", assertion),
             });
 
-            var client = new HttpClient();
-            var response = client.PostAsync(_tokenEndpoint, formContent).Result;
-            isError = !response.IsSuccessStatusCode;
-            return response.Content.ReadAsStringAsync().Result;
+            return formContent;
         }
 
         public string GetJwtAssertion()
         {
             var dateTimeOffset = new DateTimeOffset(DateTime.UtcNow);
+            string _certificateThumbPrint = (string)GetOptionValue("thumbprint");
 
-            var cert = GetCertificateFromKeyStore(_certificateThumbPrint, StoreName.My, StoreLocation.LocalMachine);
+            var cert = GetCertificateFromKeyStore(_certificateThumbPrint, StoreName.My, StoreLocation.CurrentUser);
+
             var securityKey = new X509SecurityKey(cert);
             var header = new JwtHeader(new SigningCredentials(securityKey, SecurityAlgorithms.RsaSha256))
             {
@@ -138,21 +142,48 @@ namespace AltinnCLI.Commands.Login.SubCommandHandlers
             header.Remove("typ");
             header.Remove("kid");
 
-            var payload = new JwtPayload
-            {
-                { "aud", _audience },
-                { "resource", _resource },
-                { "scope", _scopes },
-                { "iss", _issuer },
-                { "exp", dateTimeOffset.ToUnixTimeSeconds() + _tokenTtl },
-                { "iat", dateTimeOffset.ToUnixTimeSeconds() },
-                { "jti", Guid.NewGuid().ToString() },
-            };
+            var payload = new JwtPayload();
+            //{
+            //    { "aud", _audience },
+            //    { "resource", _resource },
+            //    { "scope", _scopes },
+            //    { "iss", _issuer },
+            //    { "exp", dateTimeOffset.ToUnixTimeSeconds() + _tokenTtl },
+            //    { "iat", dateTimeOffset.ToUnixTimeSeconds() },
+            //    { "jti", Guid.NewGuid().ToString() },
+            //};
 
             var securityToken = new JwtSecurityToken(header, payload);
             var handler = new JwtSecurityTokenHandler();
 
             return handler.WriteToken(securityToken);
         }
-    }
+
+        private static X509Certificate2 GetCertificateFromKeyStore(string thumbprint, StoreName storeName, StoreLocation storeLocation, bool onlyValid = false)
+        {
+            var store = new X509Store(storeName, storeLocation);
+            store.Open(OpenFlags.ReadOnly);
+            var certCollection = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, onlyValid);
+            var enumerator = certCollection.GetEnumerator();
+            X509Certificate2 cert = null;
+            while (enumerator.MoveNext())
+            {
+                cert = enumerator.Current;
+            }
+
+            return cert;
+        }
+
+        private void FindAllCeriticates()
+        {
+            var store = new X509Store(StoreName.My, StoreLocation.CurrentUser); // StoreLocation.CurrentUser); //StoreLocation.LocalMachine fails too
+            store.Open(OpenFlags.ReadOnly);
+            var certificates = store.Certificates;
+            foreach (var certificate in certificates)
+            {
+                var friendlyName = certificate.FriendlyName;
+                Console.WriteLine(friendlyName);
+            }
+        }
+}
 }
