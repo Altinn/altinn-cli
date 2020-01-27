eec5501aa70a00b5d6c49b5d06916a79a4abdf15
Håvard Hegtun <havard.hegtun@capgemini.com>
2020-01-27 13:31:43 +0100

    Adding GetInstances command definition file

---
+++ AltinnCLI/Commands/Application/SubCommandHandlers/GetInstancesSubCommandHandler.cs
@@ -0,0 +1,97 @@
+﻿using AltinnCLI.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AltinnCLI.Commands.Application.SubCommandHandlers
+{
+    class GetInstancesSubCommandHandler : SubCommandHandlerBase, ISubCommandHandler, IHelp
+    {
+        /// <summary>
+        /// Handles communication with the runtime API
+        /// </summary>
+        private IApplicationClientWrapper clientWrapper = null;
+
+        /// <summary>
+        /// Creates an instance of <see cref="CreateInstanceSubCommandHandler" /> class
+        /// </summary>
+        /// <param name="logger">Reference to the common logger that the application shall used to log log info and error information</param>
+        public GetInstancesSubCommandHandler(ILogger<GetInstancesSubCommandHandler> logger) : base(logger)
+        {
+            if (ApplicationManager.ApplicationConfiguration.GetSection("UseLiveClient").Get<bool>())
+            {
+                clientWrapper = new ApplicationClientWrapper(_logger);
+            }
+            else
+            {
+                clientWrapper = new ApplicationFileClientWrapper(_logger);
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return "GetInstances";
+            }
+        }
+
+        /// <summary>
+        /// Gets the name of the CommandProvider for the command
+        /// </summary>
+        public string CommandProvider
+        {
+            get
+            {
+                return "Application";
+            }
+        }
+
+        /// <summary>
+        /// Gets the description of the command. Will be used to generate help documentation
+        /// </summary>
+        public string Description
+        {
+            get
+            {
+                return "Gets the list of instances for an application";
+            }
+        }
+
+        /// <summary>
+        /// Gets the usage of the command. Will be used to generate help documentation
+        /// </summary>
+        public string Usage
+        {
+            get
+            {
+                return $"AltinnCLI > Application GetInstances app org\n\n" +
+                       $"\tapp <-a> \tApplication short code \n" +
+                       $"\torg <-o> \tApplication owner identification code \n";
+            }
+        }
+
+        public string GetHelp()
+        {
+            return Name;
+        }
+
+        public bool Run()
+        {
+            if (IsValid)
+            {               
+                    string app = (string)GetOptionValue("app");
+                    string org = (string)GetOptionValue("org");
+        
+                string response = clientWrapper.GetInstances(app, org);
+                _logger.LogInformation(response);
+                
+            }
+
+            return true;
+        }
+    }
+}
