2abd58b40b2b7b93a3f0a7695466730f52231260
tba76 <thomabak@gmail.com>
2022-01-26 10:46:29 +0100

    Prefill command

--- AltinnCLI/Commands/Prefill/PrefillCommand.cs
+++ AltinnCLI/Commands/Prefill/PrefillCommand.cs
@@ -1,13 +1,41 @@
-﻿using System;
+﻿using AltinnCLI.Commands.Core;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AltinnCLI.Commands.Prefill
 {
     public class PrefillCommand : ICommand, IHelp
     {
+        public string Name => "Prefill";
 
+        public string Description => $"\tProvides different prefill options.";
+
+        public string Usage => $@"Prefill <command>";
+
+        public string GetHelp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run(ISubCommandHandler commandHandler = null)
+        {
+            if (commandHandler == null)
+            {
+                Console.WriteLine();
+                Console.WriteLine(Usage);
+                Console.WriteLine();
+            }
+            else
+            {
+                commandHandler.Run();
+            }
+        }
+
+        public void Run(Dictionary<string, string> input)
+        {
+            Console.WriteLine();
+            Console.WriteLine(Usage);
+            Console.WriteLine();
+        }
     }
 }
---
+++ AltinnCLI/Commands/Prefill/SubCommandHandlers/Altinn2Handler.cs
@@ -0,0 +1,79 @@
+﻿using AltinnCLI.Commands.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AltinnCLI.Commands.Prefill.SubCommandHandlers
+{
+    public class Altinn2Handler : SubCommandHandlerBase, ISubCommandHandler, IHelp
+    {
+        public Altinn2Handler(ILogger<Altinn2Handler> logger) : base(logger)
+        {
+        }
+
+        /// <summary>
+        /// Gets the name of the command
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return "Altinn2";
+            }
+        }
+
+        /// <summary>
+        /// Gets the description of the command. Will be used to generate help documentation
+        /// </summary>
+        public string Description
+        {
+            get
+            {
+                return "Create Altinn 3 instances from Altinn 2 prefill-data";
+            }
+        }
+
+        /// <summary>
+        /// Gets the usage of the command. Will be used to generate help documentation
+        /// </summary>
+        public string Usage
+        {
+            get
+            {
+                return $"AltinnCLI > Prefill altinn2 \n\n";
+            }
+        }
+
+        /// <summary>
+        /// Gets the name of the CommandProvider for the command
+        /// </summary>
+        public string CommandProvider
+        {
+            get
+            {
+                return "Prefill";
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string GetHelp()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Processes and runs the command
+        /// </summary>
+        /// <returns>Returns true if the command completes succesfully</returns>
+        public bool Run()
+        {
+            return true;
+        }
+    }
+}
