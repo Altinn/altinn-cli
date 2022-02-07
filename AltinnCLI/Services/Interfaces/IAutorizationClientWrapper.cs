f1dd7dfa6393bedc90c8cb92f44d3a314556800e
acn-sbuad <stephanie.buadu@avanade.com>
2022-02-07 17:32:05 +0100

    removed unused services

--- AltinnCLI/Services/Interfaces/IAutorizationClientWrapper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace AltinnCLI.Services.Interfaces
-{
-    public interface IAutorizationClientWrapper
-    {
-        Task<string> ConvertToken(string token, bool test = false);
-    }
-}
--- AltinnCLI/Services/Interfaces/IMaskinPortenClientWrapper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Net.Http;
-
-namespace AltinnCLI.Services.Interfaces
-{
-    public interface IMaskinPortenClientWrapper
-    {
-        bool PostToken(FormUrlEncodedContent bearer, out string token);
-    }
-}
